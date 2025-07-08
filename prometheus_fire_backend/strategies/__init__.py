ce66806efee10aebf14a2de9c18aa487c700f8d3
hsp-1234 <3514217@gmail.com>
2025-07-09 03:20:51 +0800

    feat(ARCH): Project Olympus - Initial commit of the refactored architecture

--- prometheus_fire_backend/strategies/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file makes the 'strategies' directory a Python package.
-# You can optionally import strategy functions here for easier access, e.g.:
-# from .sma_cross_strategy import generate_signals as sma_cross_generate_signals
-
-# Or, leave it empty if dynamic loading (e.g., using importlib) is preferred
-# without explicit imports here.
